690678b785a0ceee8bd3a2a1b648cd8bcb43e77e
Dat T. Tran <datttrian@gmail.com>
2023-05-29 01:31:26 -0400

    Update create page

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,9 +1,21 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
-export const PORT = async (req) => {
+export const POST = async (req) => {
   const { userId, prompt, tag } = await req.json();
 
   try {
     await connectToDB();
-  } catch (error) {}
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Reponse("Failed to create a new prompt", { status: 500 });
+  }
 };
